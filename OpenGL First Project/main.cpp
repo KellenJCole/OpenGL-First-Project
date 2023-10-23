9838a470d4210f622476252638376cee97a423d1
Kellen Cole <kellencole03@gmail.com>
2023-10-23 16:10:15 -0700

    Initialize and tell GLFW which version & profile of OpenGL to use

--- OpenGL First Project/main.cpp
+++ OpenGL First Project/main.cpp
@@ -5,4 +5,11 @@
 
 int main() {
 
+	glfwInit(); // Initializes GLFW library. Must be done before using most GLFW functions. Returns GL_TRUE if successful, and GL_FALSE if not.
+	// "window hint": a setting or configuration option to guide GLFW on intended window behavior
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // Tell GLFW which major version of OpenGL we are using - 3.x
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // Tell GLFW which minor version of OpenGL we are using - x.3
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Tell GLFW we are using the core-profile of OpenGL; as opposed to the old compatibility
+
+	return 0;
 }
