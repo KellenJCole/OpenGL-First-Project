36187fd9550688d60c28cdd6a752d1ab8b766402
Kellen Cole <kellencole03@gmail.com>
2023-10-23 16:18:30 -0700

    Create a GLFWwindow object

--- OpenGL First Project/main.cpp
+++ OpenGL First Project/main.cpp
@@ -2,6 +2,7 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main() {
 
@@ -10,6 +11,14 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // Tell GLFW which major version of OpenGL we are using - 3.x
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // Tell GLFW which minor version of OpenGL we are using - x.3
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Tell GLFW we are using the core-profile of OpenGL; as opposed to the old compatibility
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL); // Create a window object of resolution 800x600. Leave monitor unspecified and do not share window with other windows.
+	if (window == NULL) { // glfwCreateWindow returns NULL if it failed for whatever reason.
+		std::cout << "Failed to create GLFW window\n";
+		glfwTerminate(); // Destroys all remaining windows and cursors, frees allocated resources
+		return -1;
+	}
+	glfwMakeContextCurrent(window); // Each thread can only have a single current context at a time
 
 	return 0;
 }
