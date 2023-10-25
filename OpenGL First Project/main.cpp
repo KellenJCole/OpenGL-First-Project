8d1a2ac67d7bebd0b9613994564e3da97e56418e
Kellen Cole <kellencole03@gmail.com>
2023-10-24 22:36:42 -0700

    Add GLSL vertex shader source code

--- OpenGL First Project/main.cpp
+++ OpenGL First Project/main.cpp
@@ -23,6 +23,13 @@ float vertices[] = {
 	 0.0f,  0.5f, 0.0f
 };
 
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
 int main() {
 
 	glfwInit(); // Initializes GLFW library. Must be done before using most GLFW functions. Returns GL_TRUE if successful, and GL_FALSE if not.
